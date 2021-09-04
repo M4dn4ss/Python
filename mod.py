fb6281fd63b616c86aa19fd9d59185a816bcd750
M4dn4ss <sura0046@hotmail.com>
2021-09-04 10:05:13 +0300

    Simple module

---
+++ mod.py
@@ -0,0 +1,25 @@
+'''
+Information about the module
+'''
+
+print("The module has been added")
+
+number = 10
+
+numbers = [1,2,3]
+
+person = {
+    "name":"Ali",
+    "age":"25",
+    "city":"Istanbul"
+}
+
+def func(x):
+    '''
+        Information about the function
+    '''
+    print(f'x: {x}')
+
+class Person:
+    def speak(self):
+        print("I am speaking")    
