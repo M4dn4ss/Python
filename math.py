47e339f9d204b6c78e92362d233aa2449eab412d
M4dn4ss <sura0046@hotmail.com>
2021-09-04 09:30:51 +0300

    math module

---
+++ math.py
@@ -0,0 +1,24 @@
+# Option 1
+# import math
+# import math as operation
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.factorial)
+# value = math.sqrt(49)
+# value = math.ceil(5.9)
+
+# value = operation.factorial(5)
+
+# Option 2
+# from math import *
+def sqrt(x):
+    print('x: '+str(x))
+
+from math import factorial,sqrt,ceil
+
+value = sqrt(9)
+# value = ceil(9.8)
+
+
+print(value)
