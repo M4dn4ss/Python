86e8139329a74f138484deaf1365183ef879362c
M4dn4ss <sura0046@hotmail.com>
2021-09-10 09:51:09 +0300

    iterators

---
+++ iterators.py
@@ -0,0 +1,56 @@
+
+
+# iterator = iter(listt)
+
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator)) 
+# print(next(iterator))
+# print(next(iterator))
+# # print(next(iterator))
+
+# for i in listt:
+#     print(i)
+
+# listt = [1,2,3,4,5]
+
+# iterator = iter(listt)
+
+# while True:
+#     try:
+#         element = next(iterator)
+#         print(element)
+#     except StopIteration:
+#         break    
+
+class MyNumbers:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start <= self.stop:
+            x = self.start
+            self.start += 1
+            return x    
+        else:
+            raise StopIteration
+
+listt = MyNumbers(20,50)
+
+myiter = iter(listt)
+# print(next(myiter))
+# print(next(myiter))
+
+while True:
+    try:
+        element = next(myiter)
+        print(element)
+    except StopIteration:
+        break    
+
+# for x in listt:
+#     print(x)
